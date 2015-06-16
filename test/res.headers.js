f65516a09a62e665e014e2cf12dfe6a4fa3922ba
dotnetcarpenter <jon.ronnenberg@gmail.com>
2015-06-16 17:53:43 +0200

    added failing test to check refactoring of setHeader/removeHeader

---
+++ test/res.headers.js
@@ -0,0 +1,36 @@
+
+var express = require('../')
+  , request = require('supertest');
+
+describe('res', function(){
+  describe('.setHeader("<field>", "<value>")', function(){
+    it('should set the header', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.setHeader('Expires', 'Thu, 01 Dec 1994 16:00:00 GMT').end();
+      });
+
+      request(app)
+      .get('/')
+      .end(function(err, res){
+        res.headers.should.have.property('Expires', 'Thu, 01 Dec 1994 16:00:00 GMT');
+        done();
+      })
+    });
+    it('should remove the header', function(done){
+      var app = express();
+
+      app.use(function(req, res){
+        res.removeHeader('Cache-Control');
+      });
+
+      request(app)
+      .get('/')
+      .end(function(err, res){
+        res.headers.should.not.have.property('Cache-Control');
+        done();
+      })
+    });
+  })
+})
